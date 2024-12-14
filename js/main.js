38e259f3f06d04feababd3bd08b4aff18dcf2a20
jguilldev <168439499+jguilldev@users.noreply.github.com>
2024-12-14 20:21:52 +0000

    Envio arreglo del header, con menu hamburguesa ok para index

--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
-// inicio animacion de titulo y subtitulo del hero 
+// Inicio animación de título y subtítulo del hero
 document.addEventListener("DOMContentLoaded", () => {
-    const observer = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-            observer.unobserve(entry.target); // Detener la observación una vez activada
-          }
-        });
-      },
-      {
-        threshold: 0.5, // Se activa cuando el 50% del contenedor es visible
-      }
-    );
-  
-    const content = document.querySelector(".content");
-    observer.observe(content);
-  });
-// final animacion de titulo y subtitulo del hero 
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target); // Detener la observación una vez activada
+        }
+      });
+    },
+    {
+      threshold: 0.5, // Se activa cuando el 50% del contenedor es visible
+    }
+  );
 
-// Inicio Animación Carrusel
+  const content = document.querySelector(".content");
+  if (content) {
+    observer.observe(content);
+  }
+});
+
+// Final animación de título y subtítulo del hero
+
+// Inicio animación carrusel
 document.addEventListener("DOMContentLoaded", () => {
   const carrusel = document.querySelector(".carrusel");
   const items = document.querySelectorAll(".carrusel-item");
@@ -38,7 +41,29 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// Final animación carrusel
 
-// Fin Animación Carrusel
+// Inicio funcionalidad menú hamburguesa
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector("#menu-toggle");
+  const navMenu = document.querySelector(".nav ul");
 
-  
+  if (menuToggle && navMenu) {
+    menuToggle.addEventListener("click", () => {
+      const viewportWidth = window.innerWidth;
+      if (viewportWidth < 768) {
+        navMenu.classList.toggle("active"); // Abre o cierra el menú
+      }
+    });
+
+    document.addEventListener("click", (e) => {
+      if (
+        !menuToggle.contains(e.target) && // No es el botón
+        !navMenu.contains(e.target) && // No es el menú
+        navMenu.classList.contains("active") // Y el menú está abierto
+      ) {
+        navMenu.classList.remove("active"); // Cierra el menú
+      }
+    });
+  }
+});
